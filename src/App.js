efb66359ebb70af908d051597e8e38c5c2eb22de
unique_lhy <qllinhonyu@qq.com>
2017-08-16 07:11:54 +0800

    导航html

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route,Link} from 'react-router-dom';
 
 import Index from './pages/index';
+import styles from './App.less'
 
 class App extends Component {
   render() {
     return (
         <BrowserRouter>
           <div>
-            <a href="/abc"> 首页 </a>
-            <Route path="/abc" component={Index}/>
+            <div className={styles.header}>
+              <Link to="/index"> 首页 </Link>
+              <Link to="/marryme"> 结婚页 </Link>
+              <Link to="/abc1"> 小呆专区 </Link>
+              <Link to="/abc2"> 瓜哥专区 </Link>
+              <Link to="/about"> 关于我们 </Link>
+            </div>
+            <Route path="/index" component={Index}/>
           </div>
         </BrowserRouter>
     );
