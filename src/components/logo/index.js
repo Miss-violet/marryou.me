932f25b0cac7150ba58cbc8ea1cf4ed378dc7591
wangxj <wangxj@linesum.com>
2017-08-17 17:40:37 +0800

    首页排版

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import logo from './logo.svg'
+
+import styles from './logo.css'
 
 class Logo extends Component {
     render() {
         return (
             <div>
-                logo
+                <img src={logo} alt="Logo" className={styles.logo} />
             </div>
         )
     }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Logo from '../../components/logo'
+
+import Tag from './tag.js'
+import Acticle from './acticle.js'
+import Author from './author.js'
+
+import styles from './index.css'
 
 class Header extends Component {
   render() {
     return (
       <div>
         <Logo />
-        index
+        <div className={styles.indexWrap}>
+          <Tag />
+          <Acticle />
+          <Author />
         </div>
+      </div>
     );
   }
 }
