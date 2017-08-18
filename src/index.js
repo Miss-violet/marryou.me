3c5abaea5159595c349d682dccabb7a5174f4622
wangxj <wangxj@linesum.com>
2017-08-18 14:57:05 +0800

    添加预呈现到静态HTML中

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { render } from 'react-snapshot';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
