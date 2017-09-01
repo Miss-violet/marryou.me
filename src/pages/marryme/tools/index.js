139ffde55e3b9e797cef13de56b06841963de181
wangxj <wangxj@linesum.com>
2017-09-01 18:42:03 +0800

    照片添加、工具栏交互添加

--- src/pages/marryme/tools/index.js
+++ src/pages/marryme/tools/index.js
@@ -4,51 +4,150 @@ import classnames from 'classnames'
 import toolsStyles from './styles/index.css'
 import iconFont from '../../../assets/styles/iconfont/style.css'
 
+/* 右下角图标样式 */
+const fmIcon = classnames({
+    [iconFont.icon_edit]: true,
+    [toolsStyles.editIcon]: true,
+})
+const menuIcon = classnames({
+    [iconFont.icon_menu]: true,
+    [toolsStyles.menuIcon]: true,
+})
+const topIcon = classnames({
+    [iconFont.icon_top]: true,
+    [toolsStyles.topIcon]: true,
+})
+
 class Tools extends Component {
     constructor() {
         super()
         this.state = {
             showFm: false,            // 是否展开登记表单
-            showMenu: false,         // 是否展开菜单内容
+            showMenu: false,          // 是否展开菜单内容
+            allStyle: true,           // 选择菜单项 “ 全部 ”
+            gardenStyle: false,       // 选择菜单项 “ 植物园 ”
+            zhongshanStyle: false,    // 选择菜单项 “ 中山路 ”
+            seasideStyle: false,      // 选择菜单项 “ 集美海边 ”
+            interiorStyle: false,     // 选择菜单项 “ 室内 ”
+        }
+    }
+
+
+    /* 显示登记表单/菜单 */
+    handleToShow = (type) => {
+        switch (type) {
+            case 'showFm':
+                this.setState({
+                    showFm: true,
+                    showMenu: false,
+                })
+                break
+            case 'showMenu':
+                this.setState({
+                    showFm: false,
+                    showMenu: true,
+                })
+                break
+            default:
+                this.setState({
+                    showFm: false,
+                    showMenu: false,
+                })
+                break
+        }
+    }
+    /* 切换菜单选项 */
+    handleToCrt = (type) => {
+        switch (type) {
+            case 'all':
+                this.setState({
+                    allStyle: true,
+                    gardenStyle: false,
+                    zhongshanStyle: false,
+                    seasideStyle: false,
+                    interiorStyle: false,
+                })
+                break
+            case 'garden':
+                this.setState({
+                    allStyle: false,
+                    gardenStyle: true,
+                    zhongshanStyle: false,
+                    seasideStyle: false,
+                    interiorStyle: false,
+                })
+                break
+            case 'zhongshan':
+                this.setState({
+                    allStyle: false,
+                    gardenStyle: false,
+                    zhongshanStyle: true,
+                    seasideStyle: false,
+                    interiorStyle: false,
+                })
+                break
+            case 'seaside':
+                this.setState({
+                    allStyle: false,
+                    gardenStyle: false,
+                    zhongshanStyle: false,
+                    seasideStyle: true,
+                    interiorStyle: false,
+                })
+                break
+            case 'interior':
+                this.setState({
+                    allStyle: false,
+                    gardenStyle: false,
+                    zhongshanStyle: false,
+                    seasideStyle: false,
+                    interiorStyle: true,
+                })
+                break
+            default:
+                break
+        }
+    }
+    /* 滚动置顶 */
+    handleToTop = () => {
+        window.scrollBy(0, -100)
+
+        const scrolldelay = setTimeout(this.handleToTop, 20)
+
+        var sTop = document.documentElement.scrollTop + document.body.scrollTop
+
+        if (sTop === 0) {
+            clearTimeout(scrolldelay)
         }
     }
     render() {
-        /* 菜单选中样式（先写死） */
+        /* 菜单选中样式 */
+        const allStyle = classnames({
+            [toolsStyles.menuItem]: true,
+            [toolsStyles.crtItem]: this.state.allStyle,
+        })
         const gardenStyle = classnames({
             [toolsStyles.menuItem]: true,
-            [toolsStyles.crtItem]: true,
+            [toolsStyles.crtItem]: this.state.gardenStyle,
         })
         const zhongshanStyle = classnames({
             [toolsStyles.menuItem]: true,
-            [toolsStyles.crtItem]: false,
+            [toolsStyles.crtItem]: this.state.zhongshanStyle,
         })
         const seasideStyle = classnames({
             [toolsStyles.menuItem]: true,
-            [toolsStyles.crtItem]: false,
+            [toolsStyles.crtItem]: this.state.seasideStyle,
         })
         const interiorStyle = classnames({
             [toolsStyles.menuItem]: true,
-            [toolsStyles.crtItem]: false,
-        })
-
-        /* 右下角图标样式 */
-        const fmIcon = classnames({
-            [iconFont.icon_edit]: true,
-            [toolsStyles.editIcon]: true,
-        })
-        const menuIcon = classnames({
-            [iconFont.icon_menu]: true,
-            [toolsStyles.menuIcon]: true,
-        })
-        const topIcon = classnames({
-            [iconFont.icon_top]: true,
-            [toolsStyles.topIcon]: true,
+            [toolsStyles.crtItem]: this.state.interiorStyle,
         })
 
         return (
             <div className={toolsStyles.toolsWrap}>
+                {/* 登记表单 */}
                 <div className={toolsStyles.icon}>
-                    <div className={fmIcon}></div>
+                    <div className={fmIcon} onClick={this.handleToShow.bind(this, 'showFm')}></div>
                     {
                         this.state.showFm &&
                         <div className={toolsStyles.showFm}>
@@ -85,31 +184,36 @@ class Tools extends Component {
                         </div>
                     }
                 </div>
+                {/* 菜单 */}
                 <div className={toolsStyles.icon}>
-                    <div className={menuIcon}></div>
+                    <div className={menuIcon} onClick={this.handleToShow.bind(this, 'showMenu')}></div>
                     {
                         this.state.showMenu &&
                         <div className={toolsStyles.showMenu}>
                             <span className={toolsStyles.showCircle}></span>
                             <div className={toolsStyles.menuItems}>
-                                <div className={gardenStyle}>
+                                <div className={allStyle} onClick={this.handleToCrt.bind(this, 'all')}>
+                                    <span className={toolsStyles.itemCircle}></span>全部
+                                </div>
+                                <div className={gardenStyle} onClick={this.handleToCrt.bind(this, 'garden')}>
                                     <span className={toolsStyles.itemCircle}></span>植物园
-                            </div>
-                                <div className={zhongshanStyle}>
+                                </div>
+                                <div className={zhongshanStyle} onClick={this.handleToCrt.bind(this, 'zhongshan')}>
                                     <span className={toolsStyles.itemCircle}></span>中山路
-                            </div>
-                                <div className={seasideStyle}>
+                                </div>
+                                <div className={seasideStyle} onClick={this.handleToCrt.bind(this, 'seaside')}>
                                     <span className={toolsStyles.itemCircle}></span>集美海边
-                            </div>
-                                <div className={interiorStyle}>
+                                </div>
+                                <div className={interiorStyle} onClick={this.handleToCrt.bind(this, 'interior')}>
                                     <span className={toolsStyles.itemCircle}></span>室内
-                            </div>
+                                </div>
                             </div>
                         </div>
                     }
                 </div>
+                {/* 置顶 */}
                 <div className={toolsStyles.icon} title="滚动至顶">
-                    <div className={topIcon}></div>
+                    <div className={topIcon} onClick={this.handleToTop.bind(this)}></div>
                 </div>
             </div>
         )
